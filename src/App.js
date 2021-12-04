b60986002b7ddd0c32c0dea3798c370f449f0713
Thomas Mino Pradel <tmino@imagen.cl>
2021-12-04 16:33:34 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Componente from './Componentes/Componente/Componente';
+import NavBar from './Componentes/NavBar/NavBar';
+import Carousel from './Componentes/Carousel/Carousel'
+import Card from './Componentes/Card/Card'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="body">
+      <NavBar />
+      <div className="container">
+
+        <div className="mt-1 p-5 bg-dark text-white rounded ">
+          <h1>Thomás Enrique - Portafolios</h1>
+          <p>Presentando mis habilidades profesionales</p>
+        </div>
+
+        <div className="row" style={{ marginTop: "1rem" }}>
+          <div className="col-4">
+            <Card visible={true} />
+          </div>
+          <div className="col-8">asdaczxcz</div>
+        </div>
+
+        <Carousel visible={false} />
+        <Componente visible={false} propiedad={'Variable enviada desde el padre al componente'} />
+
+        <a target="_blank" href="https://thomasenriqueportafolio.000webhostapp.com/">
+          <button classNameName="btn btn-primary" > boton</button>
         </a>
-      </header>
+
+      </div>
+
     </div>
   );
 }
